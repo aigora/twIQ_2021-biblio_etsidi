a43b5c1321f731999b7660a5e8233e027c3262da
Rq2021 <80398756+Rq2021@users.noreply.github.com>
2021-03-21 23:51:37 +0100

    Add files via upload

--- biblio_etsidi.c
+++ biblio_etsidi.c
@@ -1,26 +1,28 @@
 #include<stdio.h>
+#include<string.h>
 
 
 int main(){
 	
 	int opcion;
 	
-	
 		system("COLOR CF"); 
+		
 		printf("\n");
 		printf("\n");	
 		printf("\n");
 		printf("\n");
-		printf("                                    BIENVENIDO A TU BIBLIOTECA PERSONAL\n");
+		printf("                                      BIENVENIDO A TU BIBLIOTECA PERSONAL\n");
 		printf("\n");
 		printf("\n");
 		printf("\n");	
-		printf("                                      POR FAVOR SELECCIONA UNA OPCION\n");
+		printf("                                        POR FAVOR SELECCIONA UNA OPCION\n");
 		printf("\n");
-		printf("                                           ******************\n");
-		printf("                                           *   1.CONSULTA   *\n");
-		printf("                                           *   2.EDICION    *\n");
-		printf("                                           ******************\n");
+		printf("\n");
+		printf("                                              ******************\n");
+		printf("                                              *   1.CONSULTA   *\n");
+		printf("                                              *   2.EDICION    *\n");
+		printf("                                              ******************\n");
 		
 	
 			
@@ -32,15 +34,45 @@ int main(){
 				
 			switch(opcion){
 			case 1:
-			printf("USTED HA SELECCIONADO CONSULTAR UN DOCUMENTO");
-				break;
+					system("cls");
+					printf("HA SELECCIONADO CONSULTAR UN DOCUMENTO");
+					
+					printf("\n");
+					printf("\n");	
+					printf("\n");
+					printf("\n");
+										
+					printf("                                          POR FAVOR ELIGE UNA OPCION\n");
+					printf("\n");
+					printf("\n");
+					printf("                                        ******************************\n");
+					printf("                                        *   1.BUSQUEDA               *\n");
+					printf("                                        *   2.VOLVER AL MENU INICIO  *\n");
+					printf("                                        ******************************\n");
+					break;
 			case 2:
-			printf("USTED HA SELECCIONADO EDITAR UN DOCUMENTO");
-				break;
+					system("cls");
+					printf("HA SELECCIONADO EDITAR UN DOCUMENTO");
+										
+					printf("\n");
+					printf("\n");	
+					printf("\n");
+					printf("\n");
+										
+					printf("                                             POR FAVOR ELIGE UNA OPCION\n");
+					printf("\n");
+					printf("\n");
+					printf("                                         ***********************************\n");
+					printf("                                         *   1.NUEVA ENTRADA               *\n");
+					printf("                                         *   2.MODIFICAR DATOS/COMENTARIOS *\n");
+					printf("                                         *   3.ELIMINAR UN DOCUMENTO       *\n");
+					printf("                                         *   4.VOLVER AL MENU INICIO       *\n");
+					printf("                                         ***********************************\n");
+					break;
 					
 			}
 		
-			//system("cls");
+			
 		}
 			
 						
